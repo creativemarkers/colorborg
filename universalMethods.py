0f3446e0208b6b3339cba72bf382c95846584bea
Fernando Sanchez <nerudasrs@gmail.com>
2024-04-09 22:26:22 -0400

    worked on clickOnBankerBooth function, still need to flesh out verifyBankBooth, and boothBanker

--- universalMethods.py
+++ universalMethods.py
@@ -28,28 +28,41 @@ class Uni:
                 pass
         pass
 
-    def bankBoother(self, bankerColor):
-        #function that handles banking at a bank booth
-        verificationText = "Bank Bank Booth"
-        while not self.banking():
-            try:
-                self.findBanker(bankerColor)
-            except ImageNotFoundException:
-                pass
-
-    def banking(self)->bool:
+    def checkIfBanking(self)->bool:
         #returns bool for to check if banking
         return pyautogui.pixelMatchesColor(550,707,(38,250,43))
-
 
     def findBanker(self,bankerColor:tuple)->int:
         #assumes Banker's in camera
         matchingPixels = self.mouse.findColorsRandomly(bankerColor)
         y, x = matchingPixels[0]
-        return y,x
+        return x, y
     
     def verifyBanker():
         pass
 
     def bankItems():
+        pass
+
+    def clickOnBankerBooth(self, bankerColor):
+    #function that handles banking at a bank booth
+        print("UNIVERSALMETHODS:CLICKONBANKERBOOTH: Attempting to click on bank booth...")
+        verificationText = "Bank Bank Booth"
+        while not self.checkIfBanking():
+            attempts = 0
+            maxAttempts = 4
+            while attempts <= maxAttempts:
+                if attempts == 4:
+                    self.mouse.rotateCameraInRandomDirection("upleft", weightAmount=10)
+                try:
+                    x, y= self.findBanker(bankerColor)
+                    attempts = 5 
+                except ImageNotFoundException:
+                    attempts += 1
+            randDur = random.uniform(0.4,0.7)
+            x, y = self.mouse.moveMouseToArea(x,y,duration=randDur,areaVariance=5)
+            if self.verifyBanker(verificationText):
+                self.mouse.mouseClick(x,y)
+
+    def boothBanker():
         pass
